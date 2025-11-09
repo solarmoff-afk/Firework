63d478a6ad77cef799298f7b491ebfb98b4251a3
UpdateDeveloper <solarmoff@gmail.com>
2025-11-09 17:42:13 +0300

    Сделал передачу MoonBridge в менеджер дерева виджетов, сделал тестовый кейс для прямоугольника

--- firework/src/lib.rs
+++ firework/src/lib.rs
@@ -6,13 +6,11 @@ pub mod prelude;
 mod widget_tree;
 mod moon_bridge;
 
-use std::collections::HashMap;
-use element::{Element, ElementId};
+use element::Element;
 use glam::Vec4;
-use moonwalk::ObjectId;
 
 pub use moon_bridge::MoonBridge;
-pub use widget_tree::update_tree;
+pub use widget_tree::{update_tree, FireTree};
 
 pub fn app<F>(app_func: F)
 where
@@ -21,19 +19,11 @@ where
     let root_element = app_func();
     println!("{:#?}", root_element);
 
-    // let mut tree: HashMap<ElementId, Vec<ObjectId>> = HashMap::new();
-
-    update_tree(&root_element);
+    let mut render_tree = FireTree::new();
 
     let bridge = MoonBridge::new().expect("Failed to create MoonBridge");
 
-    {
-        let mut mw = bridge.moonwalk();
-        let id = mw.new_rect();
-        mw.config_position(id, glam::Vec2::new(100.0, 100.0));
-        mw.config_size(id, glam::Vec2::new(200.0, 150.0));
-        mw.config_color(id, Vec4::new(1.0, 0.0, 0.0, 1.0));
-    }
+    update_tree(&root_element, &mut render_tree, &bridge);
 
     bridge.run();
 }
--- firework/src/widget_tree.rs
+++ firework/src/widget_tree.rs
@@ -1,24 +1,54 @@
-pub use crate::element::{Element, ElementKind};
+use std::collections::HashMap;
+use moonwalk::{MoonWalk, ObjectId};
+use glam::Vec4;
 
-pub fn update_tree(tree: &Element) {
-    process_element(tree);
+use crate::moon_bridge::MoonBridge;
+
+pub use crate::element::{Element, ElementKind, ElementId};
+
+pub struct FireTree {
+    pub tree: HashMap<ElementId, Vec<ObjectId>>,
 }
 
-fn process_element(element: &Element) {
+impl FireTree {
+    pub fn new() -> Self {
+        Self {
+            tree: HashMap::new()
+        }
+    }
+}
+
+pub fn update_tree(root_element: &Element, render_tree: &FireTree, bridge: &MoonBridge) {
+    let mut mw = bridge.moonwalk();
+    process_element(root_element, render_tree, &mut mw);
+}
+
+fn process_element(element: &Element, render_tree: &FireTree, mw: &mut MoonWalk) {
+    let object_exists = render_tree.tree.contains_key(&element.id);
     let kind = &element.kind;
     
     /*
         Используем match вместо простого сравнения так как
-        ElementKid::Container это вариативная структура
+        ElementKind::Container это вариативная структура
         и её нельзя сравнить через if
     */
 
     match kind {
-        ElementKind::Container { children, .. } => {
-            for child in children {
-                process_element(&child);
+        ElementKind::Rect { roundedness } => {
+            if !object_exists {
+                let id = mw.new_rect();
+                mw.config_position(id, glam::Vec2::new(100.0, 100.0));
+                mw.config_size(id, glam::Vec2::new(200.0, 150.0));
+                mw.config_color(id, Vec4::new(0.0, 1.0, 0.0, 1.0));
             }
         },
+
+        ElementKind::Container { children, .. } => {
+            for child in children {
+                process_element(&child, render_tree, mw);
+            }
+        },
+
         _ => {}
     }
 }
