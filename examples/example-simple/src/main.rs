f391e7595bd29d36eab7a4647b62b5f0248e8173
UpdateDeveloper <solarmoff@gmail.com>
2026-03-10 11:37:52 +0300

    wip(compiler)

--- examples/example-simple/src/main.rs
+++ examples/example-simple/src/main.rs
@@ -1,13 +1,18 @@
-// Часть проекта Firework с открытым исходным кодом.
-// Лицензия EPL 2.0, подробнее в файле LICENSE. Copyright (c) 2026 MoonWalk
-
 use firework::ui;
 
 fn main() {
     ui! {
-        if a == 5 || a == 10 {
-            let signal = signal!(10);
-            signal += 1;
+        let signal = signal!(10);
+
+        {
+            rect!()
+                .width_percent(100);
         }
+
+        if signal > 9 {
+            text!("Hello world!").font_size(signal);
+        }
+
+        signal = 5;
     }
 }
